ef6a7d4398c81e2eeed32d95da16a69c14d41844
ssf <2022111394@bupt.cn>
2024-03-06 19:04:43 +0800

    init commit

---
+++ openpoints/models/backbone/__init__.py
@@ -0,0 +1,15 @@
+from .pointnet import PointNetEncoder
+from .pointnetv2 import PointNet2Encoder, PointNet2Decoder, PointNetFPModule
+from .pointnext import PointNextEncoder, PointNextDecoder
+from .pointmetabaselineepe import PointMetaBaselineEPEEncoder
+from .pointmetabaselineepedp import PointMetaBaselineEPEDPEncoder
+from .pointmetabaselinenope import PointMetaBaselineNOPEEncoder
+from .pointmetabase import PointMetaBaseEncoder
+
+from .dgcnn import DGCNN
+from .deepgcn import DeepGCN
+from .pointmlp import PointMLPEncoder, PointMLP
+from .pointvit import PointViT, PointViTDecoder 
+from .pct import Pct
+from .curvenet import CurveNet
+from .simpleview import MVModel
